664886ef1f9e4101af2085c73b7af7a9e971b542
Jason Dreyzehner <jason@bitpay.com>
2015-10-08 16:39:39 -0400

    fork copay-website

---
+++ README.md
@@ -0,0 +1,33 @@
+[![Build Status](https://travis-ci.org/bitpay/bitcore-website.svg)](https://travis-ci.org/bitpay/bitcore-website) [![devDependency Status](https://david-dm.org/bitpay/bitcore-website/dev-status.svg)](https://david-dm.org/bitpay/bitcore-website#info=devDependencies) [![Stories in Ready](https://badge.waffle.io/bitpay/bitcore-website.png?label=ready&title=Ready)](https://waffle.io/bitpay/bitcore-website)
+
+# Developing
+
+```sh
+$ npm install -g gulp
+$ npm install
+$ gulp serve
+```
+
+## Build
+
+```sh
+$ gulp
+```
+
+Build and optimize the site, ready for deployment. This includes linting as well as image, script, stylesheet and HTML optimization and minification.
+
+## Serve Production Build
+
+```sh
+$ gulp serve:dist
+```
+
+Serve the optimized and minified version of the site for local testing.
+
+## Deploy to gh-pages
+
+```sh
+$ gulp deploy
+```
+
+This builds for production, then deploys the dist folder to gh-pages.
---
+++ src/_scripts/init.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+  InstantClick.on('change', function() {
+    jQuery(document).foundation();
+    initSmoothScroll();
+    window.analytics.page();
+  });
+  InstantClick.init();
+
+  var wow = new window.WOW({
+    'animateClass': 'animated',
+    'offset': 100,
+    // 'callback': function(box) {
+    //   console.log('WOW: animating <' + box.tagName.toLowerCase() + '>');
+    // }
+  });
+  wow.init();
+
+  function initSmoothScroll(){
+    //smooth scrolling on clicks
+    jQuery('a[href*=#]').on('click', function(e) {
+      if (this.href.indexOf('#') === 0) {
+        e.preventDefault();
+      }
+      //don't scroll on accordion toggle
+      if(this.parentNode.className.split('accordion-navigation').length < 2){
+        jQuery('body').animate({
+          scrollTop: parseInt(jQuery('' + this.href.substring(this.href.indexOf('#'))).offset().top)
+        }, 500);
+      }
+    });
+
+    //smooth scrolling on pageloads
+    jQuery(function() {
+      if (window.location.hash) {
+        jQuery('body').animate({
+          scrollTop: parseInt(jQuery('' + window.location.href.substring(window.location.href.indexOf('#'))).offset().top)
+        }, 500);
+      }
+    });
+  }
+
+}());
