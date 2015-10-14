c34928082b635efa91f2ce7cea2ff65773b7b05a
Jason Dreyzehner <jason@bitpay.com>
2015-10-14 11:51:51 -0400

    finish routing, setup 404

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 
 var express = require('express');
 var compression = require('compression');
+var errorHandler = require('express-error-handler');
+var handler = errorHandler({
+    static: {
+      '404': 'dist/404.html'
+    }
+  });
+
 var app = express();
-
-app.set('port', (process.env.PORT || 3000));
-
+app.set('port', (process.env.PORT || 5000));
 app.use(compression());
-app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(__dirname + '/dist', {
+  extensions: 'html'
+}));
+app.use(errorHandler.httpError(404));
+app.use(handler);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
