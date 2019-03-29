dce1a86266e3808b80ee8f86f70ed8c89a411f87
Matias Alejo Garcia <ematiu@gmail.com>
2019-03-29 14:48:05 -0300

    Update 2019-03-29T17:48:04.347Z

--- tasks/delete.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var del = require('del');
-
-gulp.task('delete', function() {
-  return del(['dist']);
-});
--- tasks/playground.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-gulp.task('playground', function() {
-  gulp.src('components/bitcore-playground/**', {
-    base: 'components/bitcore-playground/'
-  }).pipe(gulp.dest('dist/playground'));
-});
